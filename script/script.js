9c224522268e56a4c30270b5399ef4a65d139db1
alancptk7 <alancptk.7@gmail.com>
2023-09-26 14:25:21 -0300

    update functions script.js

--- script/script.js
+++ script/script.js
@@ -1,41 +1,30 @@
+const element = document.getElementById('resultado');
+
 function insert(num) {
+    const textElement = element.textContent;
 
-    var numero = document.getElementById('resultado').innerHTML;
-
-    if (numero.length < 8) {
-        document.getElementById('resultado').innerHTML = numero + num;
+    if (textElement.length < 8) {
+        element.textContent = textElement + num;
     }
 }
 
 function clean() {
-
-    document.getElementById('resultado').innerHTML = "";
+    element.textContent = '';
 }
 
 function back() {
-
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+    element.textContent = element.textContent.substring(0, element.textContent.length - 1);
 }
 
 function calc() {
+    var calc = element.textContent.replace('x', '*');
+    calc = calc.replace('÷', '/');
 
-    var resultado = document.getElementById('resultado').innerHTML;
-
-    if (resultado) {
-        document.getElementById('resultado').innerHTML = eval(resultado).toFixed(1);
+    if (calc) {
+        element.textContent = eval(calc).toFixed(2);
     }
 }
 
 function change() {
-
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = eval(-numero);
+    element.textContent = eval(-element.textContent);
 }
-
-function length() {
-
-    var length = document.getElementById('resultado').innerHTML.length;
-    console.log(length);
-}
-
